0e29dc785b60685a4442601fee38ac400de94f7a
Brandon Neri <arcaniteamp@gmail.com>
2017-06-11 12:29:42 -0500

    Muchas weas

--- src/app/pages/profesor/profesor-online-class-detail/profesor-online-class-detail.component.ts
+++ src/app/pages/profesor/profesor-online-class-detail/profesor-online-class-detail.component.ts
@@ -22,6 +22,8 @@ export class ProfesorOnlineClassDetailComponent implements OnInit, OnDestroy {
 
   private allClients: any[];
   private videoList: any[];
+  private msgList: any[];
+  private chatInput: string;
 
   private webrtc: any;
 
@@ -37,6 +39,8 @@ export class ProfesorOnlineClassDetailComponent implements OnInit, OnDestroy {
   ) {
     this.allClients = [];
     this.videoList = [];
+    this.msgList = [];
+    this.chatInput = '';
 
     this.webrtc = new SimpleWebRTC({
       localVideoEl: 'main-video',
@@ -45,16 +49,30 @@ export class ProfesorOnlineClassDetailComponent implements OnInit, OnDestroy {
       nick: this.auth.user.firstName + ' ' + this.auth.user.lastName
     });
 
-    this.webrtc.on('readyToCall', () => {
-      this.webrtc.joinRoom('59382ce038964576097981d1');
-    });
-
     this.webrtc.on('videoAdded', (video, peer) => {
       this.allClients.push(peer.nick);
     });
 
     this.webrtc.on('videoRemoved', (video, peer) => {
       this.allClients.splice(this.allClients.indexOf(peer.nick), 1);
+    });
+
+    this.webrtc.on('message', (msg) => {
+      if(msg.type != 'chat-msg') return;
+      msg.payload.isLocal = msg.payload.id == this.auth.user.id;
+      this.msgList.push(msg.payload);
+    });
+
+    this.webrtc.on('channelMessage', (msg, room, data) => {
+      if(data.type != 'chat-msg') return;
+      data.payload.isLocal = data.payload.id == this.auth.user.id;
+      this.msgList.push(data.payload);
+    });
+
+    this.webrtc.on('readyToCall', () => {
+      this.webrtc.joinRoom(this.id, (err, desc) => {
+        this.snack.open('Unido correctamente a la sala: ' + this.id, '', {duration: 4000});
+      });
     });
 
     this.router.events.subscribe(
@@ -74,6 +92,7 @@ export class ProfesorOnlineClassDetailComponent implements OnInit, OnDestroy {
         data => {
           this.loader = false;
           this.clase = data;
+          this.allClients.push(this.auth.user.firstName)
         },
         err => {
           this.loader = false;
@@ -86,16 +105,31 @@ export class ProfesorOnlineClassDetailComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.sub.unsubscribe();
+    this.webrtc.leaveRoom();
+    this.webrtc.stopLocalVideo();
+    this.webrtc.disconnect();
   }
 
   ngAfterViewInit() {
-    this.initRTC(this.id);
+    
   }
 
-  initRTC(id: string) {
-
-
-
+  sendMessage(msg: string, e: any) {
+    e.preventDefault();
+    if(!msg) return;
+    if(msg.length <= 0) return;
+    this.webrtc.sendDirectlyToAll(this.id, 'chat-msg', {
+      text: msg,
+      user: this.auth.user.firstName,
+      id: this.auth.user.id
+    });
+    this.msgList.push({
+      text: msg,
+      user: this.auth.user.firstName,
+      id: this.auth.user.id,
+      isLocal: true
+    });
+    this.chatInput = '';
   }
 
 }
